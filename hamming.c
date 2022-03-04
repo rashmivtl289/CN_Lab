d2cfd604c7f39d1704503b56848ce37d2469664d
rashmivtl289 <98947167+rashmivtl289@users.noreply.github.com>
2022-03-04 16:25:23 +0530

    Add files via upload

---
+++ hamming.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+void main()
+{
+int Data[7],i,rec[7],c1,c2,c3,c;
+printf("Enter mesage");
+scanf("%d%d%d%d",&Data[0],&Data[1],&Data[2],&Data[4]);
+Data[6]=Data[4]^Data[2]^Data[0];
+Data[5]=Data[4]^Data[1]^Data[0];
+Data[3]=Data[2]^Data[1]^Data[0];
+printf("Encoded bits are\n");
+for(i=0;i<7;i++)
+{
+printf("%d",Data[i]);
+}
+printf("\nReciver side");
+printf("\nEnter encoded bits");
+for(i=0;i<7;i++)
+{
+scanf("%d",&rec[i]);
+}
+c1=rec[6]^rec[4]^rec[2]^rec[0];
+c2=rec[5]^rec[4]^rec[1]^rec[0];
+c3=rec[3]^rec[2]^rec[1]^rec[0];
+c=c3*4+c2*2+c1;
+if(c==0)
+{
+printf("No errors\n");
+}
+else
+{
+printf("There is an error at position %d\n",c);
+if(rec[7-c]==0)
+rec[7-c]=1;
+else
+rec[7-c]=0;
+printf("Error handled\n");
+}
+}
+
---
+++ vector.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int a[10][10],n;
+int min(int a, int b)
+{
+return a<b?a:b;
+}
+void floyd()
+{
+int i,j,k;
+for(k=1;k<=n;k++)
+{
+for(i=1;i<=n;i++)
+{
+for(j=0;j<=n;j++)
+{
+a[i][j]=min(a[i][j],a[i][k]+a[k][j]);
+}
+}
+}
+
+
+}
+void main()
+{
+int i,j,k;
+printf("Enter number of nodes");
+scanf("%d",&n);
+printf("Enter matrics");
+for(i=1;i<=n;i++)
+for(j=1;j<=n;j++)
+{
+scanf("%d",&a[i][j]);
+if(a[i][j]==0)
+a[i][j]=999;
+if(a[i]==a[j])
+a[i][j]=0;
+}
+floyd();
+printf("Distance matrics:\n");
+for(i=1;i<=n;i++)
+{
+for(j=1;j<=n;j++)
+{
+printf("%d ",a[i][j]);
+}
+printf("\n");
+}
+}
